714e9243b1181bfe85466ef9ee0e288aa7d07938
Jean Baptiste Favre <debian@jbfavre.org>
2016-05-20 18:40:56 +0200

    Add TweetDeck support

--- data/content-script.js
+++ data/content-script.js
@@ -1,20 +1,27 @@
-var linkClassName = "twitter-timeline-link"
+var linkClassName = [ "twitter-timeline-link", "url-ext" ];
 
 function expandCurrentTwitterLinks(){
-  var links = document.getElementsByClassName(linkClassName)
-  for(var i = 0; i < links.length; i++){
-    expandTwitterLink(links[i])
+  for(var j = 0; j < linkClassName.length; j++) {
+    var links = document.getElementsByClassName(linkClassName[j])
+    for(var i = 0; i < links.length; i++){
+      expandTwitterLink(links[i]);
+    }
   }
 }
 
 function expandAddedTwitterLinks(){
   var observer = new MutationObserver(function(mutations){
     mutations.forEach(function(mutation){
-      var addedNodes = mutation.addedNodes
-      for(var i = 0; i < addedNodes.length; i++){
-        var links = addedNodes[i].querySelectorAll("." + linkClassName)
-        for(var j = 0; j < links.length; j++){
-          expandTwitterLink(links[j])
+      var addedNodes = mutation.addedNodes;
+      if (addedNodes) {
+        for(var i = 0; i < addedNodes.length; i++){
+          for(var j = 0; j < linkClassName.length; j++) {
+            console.log(addedNodes[i]);
+            var links = addedNodes[i].querySelectorAll("." + linkClassName[j]);
+            for (var k = 0; k < links.length; k++){
+              expandTwitterLink(links[k]);
+            }
+          }
         }
       }
     })
@@ -27,12 +34,13 @@ function expandAddedTwitterLinks(){
 
 function expandTwitterLink(linkElem){
   if(linkElem.getAttribute("href")){
-    var originalLink = linkElem.getAttribute("data-expanded-url")
+    var originalLink = linkElem.getAttribute("data-expanded-url") ||
+                       linkElem.getAttribute("data-full-url");
     if(originalLink){
-      linkElem.setAttribute("href", originalLink)
+      linkElem.setAttribute("href", originalLink);
     }
   }
 }
 
-expandAddedTwitterLinks()
-expandCurrentTwitterLinks()
+expandAddedTwitterLinks();
+expandCurrentTwitterLinks();
